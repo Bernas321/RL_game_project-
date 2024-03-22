a60c0f33e7bae0d52104546f0c0c9011c52c44c1
linjulie <linjuliefjeldvik@gmail.com>
2024-03-22 10:54:34 +0100

    Continues on implementation following tutoral

--- blackjack_gym.py
+++ blackjack_gym.py
@@ -11,4 +11,63 @@ from tqdm import tqdm
 import gymnasium as gym
 
 # blackjack environment
-env = gym.make("Blackjack-v1", sab=True)
+env = gym.make('Blackjack-v1', natural=False, sab=False)
+
+
+# reset the environment to get the first observation
+done = False
+observation, info = env.reset()
+
+# observation = (16, 9, False)
+# observation is a 3-tuple consisting of 3 values:
+# - The players current sum
+# - Value of the dealers face-up card
+# - Boolean whether the player holds a usable ace (An ace is usable if it counts as 11 without busting)
+
+
+class BlackjackAgent:
+    def __init__(
+            self, 
+            learning_rate: float,
+            initial_epsilon: float,
+            epsilon_decay: float, 
+            final_epsilon: float, 
+            discount_factor: float = 0.95,
+    ):
+        """Initialize a Reinforcement Learning agent with an empty dictionary
+        of state-action values (q_values), a learning rate and an epsilon.
+
+        Args:
+            learning_rate: The learning rate
+            initial_epsilon: The initial epsilon value
+            epsilon_decay: The decay for epsilon
+            final_epsilon: The final epsilon value
+            discount_factor: The discount factor for computing the Q-value
+        """
+
+        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+
+
+
+    def get_action(self, obs: tuple[int, int, bool]) -> int:
+        """
+        Returns the best action with probability (1 - epsilon)
+        otherwise a random action with probability epsilon to ensure exploration.
+        """
+        # with probability epsilon return a random action to explore the environment
+        if np.random.random() < self.epsilon:
+            return env.action_space.sample()
+
+        # with probability (1 - epsilon) act greedily (exploit)
+        else:
+            return int(np.argmax(self.q_values[obs]))
+
